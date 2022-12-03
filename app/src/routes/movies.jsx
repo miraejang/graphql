bbf6295f8959844f549c3f443cabc8f0656aca7a
miraejang <mirae611@nate.com>
2022-12-04 01:03:51 +0900

    ApolloProvider

--- app/src/routes/movies.jsx
+++ app/src/routes/movies.jsx
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import { gql, useApolloClient } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
 
-class Movies extends Component {
-  render() {
-    return <div>Movies</div>;
-  }
-}
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const client = new useApolloClient();
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then(result => setMovies(result.data.allMovies));
+  }, [client]);
+
+  return (
+    <ul>
+      {movies.map(movie => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default Movies;
