8575e571fcf6eba4db9312f24b18ac8ddc7ff222
miraejang <mirae611@nate.com>
2022-12-04 00:23:25 +0900

    Apollo client

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Movies from './routes/movies';
+import Movie from './routes/movie';
 
 function App() {
-  return <Movies />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
